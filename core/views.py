220ff3fc6300c351c3e16ce421a8bd8d49915341
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-11-22 16:12:43 +0530

    initial release

--- core/views.py
+++ core/views.py
@@ -9,15 +9,16 @@ from . import scheduler
 
 views = Blueprint('views',__name__)
 
+# task to perform
 def task(*args):
     persons = args[0]
     image_folder = args[1]
     group_id = args[2]
     birth_day_persons = []
     today = date.today()
+
     for i in persons:
         if isinstance(i['dob'],str):
-            print(i['dob'])
             i['dob'] = datetime.strptime(i['dob'],'%d/%m/%Y')
         if i['dob'].day == today.day and i['dob'].month == today.month:
             birth_day_persons.append(i)
@@ -26,28 +27,42 @@ def task(*args):
     for i in birth_day_persons:
         message = i['caption']
         image_path = "{}/{}".format(image_folder,i['image'])
-        print(image_path)
-        sendwhats_image(group_id,image_path,message,60,20)
+        sendwhats_image(group_id,image_path,message,30,20)
 
 
-# to start sheduler path to xl file and time message
-@views.route('/start',methods=['POST'])
+# to start scheduler
+@views.route('/schedule',methods=['POST'])
 def schedule_task():
+    # input data check
     data = request.json
+
+
+    if 'job' in data and data['job'] == 'stop':
+        if scheduler.state:
+            scheduler.shutdown()
+            return {'status':'scheduled job is stopped successfully'}
+        else:
+            return {'status':'scheduled job is altready stopped'}
+
+    if 'path' not in data or 'image folder' not in data or 'group id' not in data or 'time' not in data:
+        return {'status':'check given details'}
+
     file_path = data['path']
     folder = data['image folder']
     group_id = data['group id']
     hour = data['time'][:2]
     minute = data['time'][3:5]
-    if data['job'] == 'stop'  and scheduler.state:
-        scheduler.shutdown()
-        return data
 
+    # xml file to python dictionary
     xml_file = (openpyxl.load_workbook(file_path)).active
     first_row = list(xml_file.rows)[0]
     keys =[]
     for i in first_row:
-        keys.append(i.value)
+        key = (i.value).lower()
+        if 'birth' in key or 'dob' in key:
+            keys.append('dob')
+        else:
+            keys.append(key)
     persons = []
     for row in xml_file.iter_rows(2, xml_file.max_row):
         xml_data = {}
@@ -55,11 +70,10 @@ def schedule_task():
             xml_data[keys[i]] = row[i].value
         persons.append(xml_data)
 
+    # schedule a job
     scheduler.remove_all_jobs()
-
     scheduler.add_job(func=task,args=[persons,folder,group_id], trigger='cron',day_of_week='mon-sun',hour=hour,minute=minute,id='dob message')
-    h = scheduler.get_job('dob message')  
-    print(h)
+    # start scheduler if not started
     if not scheduler.state:
         scheduler.start()
-    return {'status':'success'}
+    return {'status':'your job was scheduled at {} everyday'.format(data['time'])}

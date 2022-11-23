533ae2bad126a95581ce43708eb97963431cc070
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-11-23 20:03:27 +0530

    interval check

--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
 from flask import Blueprint
 from flask import jsonify,request
 import openpyxl
-from datetime import date,datetime
+from datetime import date,datetime,timedelta
 from flask_apscheduler import APScheduler
 import time
 from pywhatkit.whats import sendwhats_image,sendwhatmsg_instantly
 from . import scheduler
+import xlwt
+from xlwt import Workbook
+from urllib.request import urlopen,Request
+import pandas
+
+
 
 views = Blueprint('views',__name__)
 
@@ -14,20 +20,39 @@ def task(*args):
     persons = args[0]
     image_folder = args[1]
     group_id = args[2]
+    file = args[3]
+    book = args[4]
     birth_day_persons = []
     today = date.today()
+    next_day = today + timedelta(days=1)
+    max_column = file.max_column
 
+    try:
+        scheduler.remove_job('check')
+    except:
+        pass
+
+    start = "{}-{}-{} 09:00:00".format(next_day.year,next_day.month,next_day.day)
+    end = "{}-{}-{} 13:00:00".format(next_day.year,next_day.month,next_day.day)
+    print(start,end)
+    scheduler.add_job(func=task,args=[persons,image_folder,group_id,file,book],trigger='interval',hours=1,start_date=start,end_date=end,id='check')
     for i in persons:
         if isinstance(i['dob'],str):
             i['dob'] = datetime.strptime(i['dob'],'%d/%m/%Y')
         if i['dob'].day == today.day and i['dob'].month == today.month:
             birth_day_persons.append(i)
 
-    print(birth_day_persons ,'hi')
+
     for i in birth_day_persons:
         message = i['caption']
         image_path = "{}/{}".format(image_folder,i['image'])
-        sendwhats_image(group_id,image_path,message,30,20)
+        try:
+            sendwhats_image(group_id,image_path,message,30,20)
+            print('done')
+            file.cell(i['row'],max_column,'done')
+        except:
+            pass
+    book.save('details.xlsx')
 
 
 # to start scheduler
@@ -35,7 +60,9 @@ def task(*args):
 def schedule_task():
     # input data check
     data = request.json
-
+    # start scheduler if not started
+    if not scheduler.state:
+        scheduler.start()
 
     if 'job' in data and data['job'] == 'stop':
         if scheduler.state:
@@ -46,7 +73,7 @@ def schedule_task():
 
     if 'path' not in data or 'image folder' not in data or 'group id' not in data or 'time' not in data:
         return {'status':'check given details'}
-
+    
     file_path = data['path']
     folder = data['image folder']
     group_id = data['group id']
@@ -54,7 +81,17 @@ def schedule_task():
     minute = data['time'][3:5]
 
     # xml file to python dictionary
-    xml_file = (openpyxl.load_workbook(file_path)).active
+    # xml_book = openpyxl.load_workbook(file_path)
+
+    # req = Request(
+    #     url=file_path, 
+    #     headers={'User-Agent': 'Mozilla/5.0'}
+    # )
+    # socket = urlopen(req)
+    # xml_book = pandas.ExcelFile(socket)
+
+    xml_book = pandas.read_excel(file_path,engine='openpyxl')
+    xml_file = (xml_book).active
     first_row = list(xml_file.rows)[0]
     keys =[]
     for i in first_row:
@@ -64,16 +101,32 @@ def schedule_task():
         else:
             keys.append(key)
     persons = []
+    row_position = 1
     for row in xml_file.iter_rows(2, xml_file.max_row):
+        row_position += 1
         xml_data = {}
+        print(row)
         for i in range(0,len(row)):
             xml_data[keys[i]] = row[i].value
+        xml_data['row'] = row_position
         persons.append(xml_data)
 
+    if data['job'] == 'force start':
+        on_time = datetime.now() + timedelta(minutes=1)
+        print(on_time.hour,on_time.minute)
+        scheduler.add_job(func=task,args=[persons,folder,group_id,xml_file,xml_book], trigger='date',run_date=datetime(on_time.year, on_time.month, on_time.day, on_time.hour, on_time.minute,on_time.second),id='sudden dob message')
+
+        return {'data':"success"}
+
     # schedule a job
-    scheduler.remove_all_jobs()
-    scheduler.add_job(func=task,args=[persons,folder,group_id], trigger='cron',day_of_week='mon-sun',hour=hour,minute=minute,id='dob message')
-    # start scheduler if not started
-    if not scheduler.state:
-        scheduler.start()
+
+    # scheduler.remove_all_jobs()
+    # scheduler.add_job(func=task,args=[persons,folder,group_id,xml_file,xml_book], trigger='cron',day_of_week='mon-sun',hour=hour,minute=minute,id='dob message')
     return {'status':'your job was scheduled at {} everyday'.format(data['time'])}
+
+
+@views.route('/check',methods=['POST'])
+def schedule_check():
+    jobs = scheduler.get_jobs()
+    print(jobs)
+    return {'status': jobs}
